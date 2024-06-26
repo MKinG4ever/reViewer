e1eb3935a2a786c5de0a604762f53f1e3d397edc
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-06-26 14:26:17 +0330

    main
    upgrade

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from Viewer import viewer
+from Viewer import Viewer
 
 
 def main():
     # Visual input
-    url = input('Enter URL (Ex: www.google.com): ')
-    views = int(input('How many views: '))
+    _url = input('Enter URL (Ex: www.google.com): ')
+    _views = int(input('How many views: '))
+    _delay = 2  # seconds
 
-    # Number of page/site views
-    for i in range(views):
-        # Representation
-        print(f"[{i + 1}]: ", end='')
-        viewer(url, secure=False)
+    # Create a Viewer instance for 'www.google.com' using HTTPS
+    site = Viewer(url=_url, secure=True)
+
+    # View 4 pages with a delay of 1 second between each view, and print each page content
+    pages_https = site.view_pages(views=_views, delay=_delay, verbose=True)
 
 
 if __name__ == '__main__':
