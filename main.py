7b6ef09ac770a33157a0890c9ad624d20b81ca6e
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-06-30 00:13:42 +0330

    main v1.1
    update question task

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ def main():
     """
     Main function
     Timestamp 1719490798.5711012
-    Version 1.0
+    Version 1.1
 
     :return:
     """
     # Visual input
-    _url = input('Enter URL (Ex: www.google.com): ')
-    _views = int(input('How many views: '))
-    _delay = int(input('Delay between views: '))
+    _url = quest('Enter URL', 'www.google.com')
+    _secure = eval(quest('Do you require HTTPS', True))
+    _views = int(quest('How many views', 5))
+    _delay = int(quest('Delay between views in seconds: ', 60))
 
     # Create a Viewer instance for 'www.google.com' using HTTPS
-    site = Viewer(url=_url, secure=True)
+    site = Viewer(url=_url, secure=_secure)
+
+    # Presentation
+    print('\n', site, 'Initiation...', sep='\n')
 
     # View 4 pages with a delay of 1 second between each view, and print each page content
     pages_https = site.view_pages(views=_views, delay=_delay, verbose=True)
+
+
+def quest(question: str, answer):
+    a = input(f"{question} (Default: {answer}): ")
+    return a if a.strip() else answer
 
 
 if __name__ == '__main__':
