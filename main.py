d2fe9506f2139050a38b54afab74195c3ab0a3a2
NightFox <87749296+MKinG4ever@users.noreply.github.com>
2024-07-12 01:59:52 +0330

    main v1.2
    update

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ def main():
     """
     Main function
     Timestamp 1719490798.5711012
-    Version 1.1
+    Version 1.2
 
     :return:
     """
     # Visual input
-    _url = quest('Enter URL', 'www.google.com')
-    _secure = eval(quest('Do you require HTTPS', True))
-    _views = int(quest('How many views', 5))
+    _url = quest('Enter full URL', 'https://www.google.com')
+    _views = int(quest('How many views', 15))
     _delay = int(quest('Delay between views in seconds: ', 60))
 
     # Create a Viewer instance for 'www.google.com' using HTTPS
-    site = Viewer(url=_url, secure=_secure)
+    site = Viewer(url=_url)
 
     # Presentation
-    print('\n', site, 'Initiation...', sep='\n')
+    print('\n', site, '[:] Initiation...\n', sep='\n')
 
     # View 4 pages with a delay of 1 second between each view, and print each page content
     pages_https = site.view_pages(views=_views, delay=_delay, verbose=True)
